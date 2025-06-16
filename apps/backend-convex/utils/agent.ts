7f2592d2846713a6b461e6040cbfb98a1fb6b2fc
NamesMT <dangquoctrung123@gmail.com>
2025-06-16 14:05:26 +0000

    fix: new models doesn't work

--- apps/backend-convex/utils/agent.ts
+++ apps/backend-convex/utils/agent.ts
@@ -1,26 +1,21 @@
 import type { AgentObject } from '@local/common/src/aisdk'
 import type { LanguageModelV1 } from '@openrouter/ai-sdk-provider'
 import { createOpenRouter, openrouter } from '@openrouter/ai-sdk-provider'
-import { ConvexError } from 'convex/values'
 
 export function getAgentModel({ provider, model, apiKey }: AgentObject): LanguageModelV1 {
   if (provider === 'hosted') {
-    if (!['qwen3-32b', 'deepseek-v3'].includes(model))
-      throw new ConvexError('Invalid model for hosted provider')
-
-    if (model === 'qwen3-32b')
-      model = 'qwen/qwen3-32b:free'
-
-    if (model === 'deepseek-v3')
-      model = 'deepseek/deepseek-chat-v3-0324:free'
-
-    if (model === 'devstral-small-2505')
-      model = 'mistralai/devstral-small:free'
-
-    if (model === 'llama-4-scout')
-      model = 'meta-llama/llama-4-scout:free'
-
-    return openrouter(model)
+    switch (model) {
+      case 'qwen3-32b':
+        return openrouter('qwen/qwen3-32b:free')
+      case 'deepseek-v3':
+        return openrouter('deepseek/deepseek-chat-v3-0324:free')
+      case 'devstral-small-2505':
+        return openrouter('mistralai/devstral-small:free')
+      case 'llama-4-scout':
+        return openrouter('meta-llama/llama-4-scout:free')
+      default:
+        throw new Error(`Invalid model for hosted provider`)
+    }
   }
   else {
     return (() => {
