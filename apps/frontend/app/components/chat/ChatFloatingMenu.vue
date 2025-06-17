19f7f3e53a05d80eeaf05593ad3b5ead1c1b508e
NamesMT <dangquoctrung123@gmail.com>
2025-06-17 07:53:40 +0000

    feat: tip for thread context menu

--- apps/frontend/app/components/chat/ChatFloatingMenu.vue
+++ apps/frontend/app/components/chat/ChatFloatingMenu.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
 import { useSidebar } from '@/lib/shadcn/components/ui/sidebar/utils'
+import { useToast } from '~/lib/shadcn/components/ui/toast'
+
+const { toast } = useToast()
+const { t } = useI18n()
+const firstTimeOpen = useLocalState('chat/firstTimeOpen', () => true)
 
 // `state` does not apply to mobile, so we uses 3 props instead
 const { isMobile, openMobile, open } = useSidebar()
 
 const isExpanded = computed(() => isMobile.value ? openMobile.value : open.value)
+
+if (firstTimeOpen.value) {
+  until(isExpanded).toBeTruthy().then(() => {
+    toast({ description: t('chat.toast.menuOpenedFirstTime') })
+    firstTimeOpen.value = false
+  })
+}
 </script>
 
 <template>
